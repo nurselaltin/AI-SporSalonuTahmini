0d8eea5d23f7007e94dad23c4f205f2d5f472dc7
nurselaltin <nurselaltin.na@gmail.com>
2025-01-27 07:25:56 +0300

    Added project.

---
+++ AI-SporSalonuTahmini/DTO/Data.cs
@@ -0,0 +1,20 @@
+﻿namespace AI_SporSalonuTahmini.DTO
+{
+    public class Data
+    {
+        public string Name { get; set; }
+        public float Age { get; set; }
+        public float MembershipDuration { get; set; }
+        public float WeeklyVisits { get; set; }
+        public float PTUsage { get; set; }
+        public float PreviouslyQuit { get; set; }
+        public bool IsContinuing { get; set; }
+
+    }
+    // Tahmin sonuçlarını saklayacak sınıf
+    public class PredictionResult
+    {
+        public string Name { get; set; }
+        public bool PredictedLabel { get; set; }  // Modelin tahmin ettiği sınıf (Devam eder mi, etmez mi?)
+    }
+}
---
+++ AI-SporSalonuTahmini/Program.cs
@@ -0,0 +1,52 @@
+﻿using AI_SporSalonuTahmini.DTO;
+using Microsoft.ML;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Verisetini oku
+        // JSON dosyasını okuma
+        string jsonFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dataset.json");
+        var jsonData = JsonConvert.DeserializeObject<List<Data>>(File.ReadAllText(jsonFilePath));
+        #endregion
+
+        #region Kategorik Verileri sayısal verilere çevir
+        var mlContext = new MLContext();
+
+        // Veriyi IDataView'e dönüştürme
+        var trainingData = mlContext.Data.LoadFromEnumerable(jsonData);
+
+        // Kategorik verileri sayısal verilere dönüştürme ve özellikleri birleştirme
+        var pipeline = mlContext.Transforms.Concatenate("Features", "Age", "MembershipDuration", "WeeklyVisits", "PTUsage", "PreviouslyQuit")  // Özellikleri birleştir
+            .Append(mlContext.BinaryClassification.Trainers.LbfgsLogisticRegression(labelColumnName: "IsContinuing", featureColumnName: "Features"));
+        #endregion
+
+        #region Modeli Eğit
+        // Modeli eğitme
+        var model = pipeline.Fit(trainingData);
+
+        // Yeni verilerle tahmin yapmak için model kullanma
+        var prediction = model.Transform(trainingData);
+        var predictions = mlContext.Data.CreateEnumerable<PredictionResult>(prediction, reuseRowObject: false).ToList();
+        #endregion
+
+        #region Sonuçları yazdır
+        // Sonuçları yazdırma
+        foreach (var pred in predictions)
+        {
+            var tt = pred.PredictedLabel.ToString();
+            string isContinue = pred.PredictedLabel == true ? "devam edecek." : "devam etmeyecek.";
+
+            Console.WriteLine($"{pred.Name}, {isContinue}");
+        }
+        int count1 = predictions.Where(c => c.PredictedLabel == true).Count();
+        int count2 = predictions.Where(c => c.PredictedLabel == false).Count();
+        Console.WriteLine("--------------------------");
+        Console.WriteLine($"Spor salonuna toplamda {count1} kişi devame edecek, {count2} kişi ise devam etmeyecek.");
+        #endregion
+
+        Console.ReadLine();
+    }
+}
